1c201c9f7e3da1f641bf82a0c1251935826a4424
LuzIsabellaPerez <44623777+LuzIsabellaPerez@users.noreply.github.com>
2019-06-18 13:34:56 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".wutang").click(function(){
+ $("#wutang").toggle();
+});
+
+
+
+//Spell #2
+
+$(".kool").click(function(){
+ $("#kool").slideToggle();
+});
+
+
+
+//Spell #3
+
+$(".pharcyde").click(function(){
+ $("#pharcyde").fadeToggle();
+});
+
+
+
